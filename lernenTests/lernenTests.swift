90790681930c4182c82fa21ae4fe74c52b894ed6
Hamzah <rootmoodrm@gmail.com>
2024-11-05 21:11:57 +0100

    Initial Commit

---
+++ lernenTests/lernenTests.swift
@@ -0,0 +1,17 @@
+//
+//  lernenTests.swift
+//  lernenTests
+//
+//  Created by Hamzah on 05.11.24.
+//
+
+import Testing
+@testable import lernen
+
+struct lernenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
