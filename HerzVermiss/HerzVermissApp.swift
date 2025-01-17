3121118d83a3dba1850d8901ad80016e29dbf8fc
Hamzah <rootmoodrm@gmail.com>
2025-01-17 21:52:48 +0100

    mitC-

---
+++ HerzVermiss/HerzVermissApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerzVermissApp.swift
+//  HerzVermiss
+//
+//  Created by Hamzah on 14.01.25.
+//
+
+import SwiftUI
+
+@main
+struct HerzVermissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cursorswift/cursorswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursorswiftApp.swift
+//  cursorswift
+//
+//  Created by Hamzah on 17.01.25.
+//
+
+import SwiftUI
+
+@main
+struct cursorswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
